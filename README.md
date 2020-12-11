8e2b0cdb426314dad23cebc31d5cbdf21b19a43d
Leonard <thongyixuen@gmail.com>
2020-12-10 21:12:29 -0500

    final commit

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# MERN Practice
-
-A social application built using MERN (MongoDB, Express.js, React.js, Node.js).
+# Practice MERN
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,27 @@
-const jwt = require("jsonwebtoken");
-const config = require("config");
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 module.exports = function (req, res, next) {
-    const token = req.header("x-auth-token");
+  // Get token from header
+  const token = req.header('x-auth-token');
 
-    if (!token) {
-        return res.status(401).json({ msg: "No, token, authorization denied" });
-    }
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
 
-    try {
-        const decoded = jwt.verify(token, config.get("jwtSecret"));
-
+  // Verify token
+  try {
+    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: 'Token is not valid' });
+      } else {
         req.user = decoded.user;
         next();
-    } catch (error) {
-        res.status(401).json({ msg: "Token is not valid" });
-    }
+      }
+    });
+  } catch (err) {
+    console.error('something wrong with auth middleware');
+    res.status(500).json({ msg: 'Server Error' });
+  }
 };
